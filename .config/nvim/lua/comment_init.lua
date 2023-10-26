f9d15eb42b0d4f45adeac2d9567282f3022ed5b3
llyyr <llyyr.public@gmail.com>
2023-10-26 14:28:54 +0530

    nvim: fix broken configs

--- .config/nvim/lua/comment_init.lua
+++ .config/nvim/lua/comment_init.lua
@@ -1,15 +1,21 @@
 local M = {
   'numToStr/Comment.nvim',
-  dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
-  opts = {
-    pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter',
+    'JoosepAlviste/nvim-ts-context-commentstring',
   },
+  event = 'BufReadPost',
 }
 
-M.config = function()
+function M.config()
+  require('Comment').setup({
+    pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+  })
+
   local ft = require('Comment.ft')
   ft.set('nasm', '; %s')
   ft.set('asm', { '// %s', '/* %s */' })
 end
 
 return M
+
--- .config/nvim/lua/treesitter_init.lua
+++ .config/nvim/lua/treesitter_init.lua
@@ -1,82 +1,71 @@
-return {
-  -- Treesitter is a new parser generator tool that we can
-  -- use in Neovim to power faster and more accurate
-  -- syntax highlighting.
-  {
-    "nvim-treesitter/nvim-treesitter",
-    version = false,
-    build = ":TSUpdate",
-    init = function(plugin)
-      -- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
-      -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
-      -- no longer trigger the **nvim-treeitter** module to be loaded in time.
-      -- Luckily, the only thins that those plugins need are the custom queries, which we make available
-      -- during startup.
-      require("nvim-treesitter.query_predicates")
-    end,
-    dependencies = {
-      { 'nvim-treesitter/nvim-treesitter-textobjects' },
-      { 'JoosepAlviste/nvim-ts-context-commentstring' },
-      { "nvim-treesitter/nvim-treesitter-textobjects" },
-    },
-    cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
-    keys = {
-      { "<c-space>", desc = "Increment selection" },
-      { "<bs>", desc = "Decrement selection", mode = "x" },
-    },
-    ---@type TSConfig
-    ---@diagnostic disable-next-line: missing-fields
-    opts = {
-      ensure_installed = { "c", "cpp", "bash", "comment", "css",
-      "dot", "glsl", "go", "html", "haskell" ,"javascript", 
-      "julia", "latex", "lua", "make", "python", "rst", 
-      "json", "yaml", "meson", "markdown", "rust", "scss"},
+local M = {
+  "nvim-treesitter/nvim-treesitter",
+  version = false,
+  build = ":TSUpdate",
+  dependencies = {
+    { 'nvim-treesitter/nvim-treesitter-textobjects' },
+    { 'JoosepAlviste/nvim-ts-context-commentstring' },
+    { "nvim-treesitter/nvim-treesitter-textobjects" },
+  },
+  cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
+  keys = {
+    { "<c-space>", desc = "Increment selection" },
+    { "<bs>", desc = "Decrement selection", mode = "x" },
+  },
+}
 
-      ignore_install = { },        -- List of parsers to ignore installing
-      refactor = {
+M.config = function()
+require('nvim-treesitter.configs').setup({
+    -- one of "all", "maintained", or a list
+    ensure_installed = { "c", "cpp", "bash", "comment", "css",
+                         "dot", "glsl", "go", "html", "haskell" ,"javascript", 
+                         "julia", "latex", "lua", "make", "python", "rst", 
+                         "json", "yaml", "meson", "markdown", "rust", "scss"},
+    ignore_install = { },        -- List of parsers to ignore installing
+    refactor = {
         highlight_definitions   = { enable = false },
         highlight_current_scope = { enable = false },
         smart_rename = {
-          enable = true,
-          keymaps = {
-            smart_rename = "grr",
-          },
+            enable = true,
+            keymaps = {
+                smart_rename = "grr",
+            },
         },
-      },
-      textobjects = {
+    },
+    textobjects = {
         move = {
-          enable    = true,
-          set_jumps = true, -- whether to set jumps in the jumplist
-          goto_next_start = {
-            ["]m"] = "@function.outer",
-            ["]]"] = "@class.outer",
-          },
-          goto_next_end = {
-            ["]M"] = "@function.outer",
-            ["]["] = "@class.outer",
-          },
-          goto_previous_start = {
-            ["[m"] = "@function.outer",
-            ["[["] = "@class.outer",
-          },
-          goto_previous_end = {
-            ["[M"] = "@function.outer",
-            ["[]"] = "@class.outer",
-          },
+            enable    = true,
+            set_jumps = true, -- whether to set jumps in the jumplist
+            goto_next_start = {
+                ["]m"] = "@function.outer",
+                ["]]"] = "@class.outer",
+            },
+            goto_next_end = {
+                ["]M"] = "@function.outer",
+                ["]["] = "@class.outer",
+            },
+            goto_previous_start = {
+                ["[m"] = "@function.outer",
+                ["[["] = "@class.outer",
+            },
+            goto_previous_end = {
+                ["[M"] = "@function.outer",
+                ["[]"] = "@class.outer",
+            },
         },
         lsp_interop = {
-          enable = true,
-          border = 'single',
-          peek_definition_code = {
-            ["<leader>dF"] = "@function.outer",
-            ["<leader>df"] = "@class.outer",
-          },
+            enable = true,
+            border = 'single',
+            peek_definition_code = {
+                ["<leader>dF"] = "@function.outer",
+                ["<leader>df"] = "@class.outer",
+            },
         },
-      },
-      indent = {
+    },
+    indent = {
         enable = false,
-      },
-      highlight = {
+    },
+    highlight = {
         enable = true,         -- false will disable the whole extension
         disable = { "rst" },  -- list of language that will be disabled
 
@@ -85,35 +74,20 @@ return {
         -- Using this option may slow down your editor, and you may see some duplicate highlights.
         -- Instead of true it can also be a list of languages
         additional_vim_regex_highlighting = false,
-      },
-      context_commentstring = {
+    },
+    context_commentstring = {
         enable = true,
-        enable_autocmd = false,
-      },
-      incremental_selection = {
+    },
+    incremental_selection = {
         enable = true,
         keymaps = {
-          init_selection    = "gnn",
-          node_incremental  = "grn",
-          scope_incremental = "grc",
-          node_decremental  = "grm",
+            init_selection    = "gnn",
+            node_incremental  = "grn",
+            scope_incremental = "grc",
+            node_decremental  = "grm",
         },
-      },
-      ---@param opts TSConfig
-      config = function(_, opts)
-        if type(opts.ensure_installed) == "table" then
-          ---@type table<string, boolean>
-          local added = {}
-          opts.ensure_installed = vim.tbl_filter(function(lang)
-            if added[lang] then
-              return false
-            end
-            added[lang] = true
-            return true
-          end, opts.ensure_installed)
-        end
-        require("nvim-treesitter.configs").setup(opts)
-      end,
     },
-  }
-}
+})
+end
+
+return M
